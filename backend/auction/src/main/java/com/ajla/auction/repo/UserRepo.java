ab8fe14e6b4c2a6b29870c0b74bfe08813e526c9
amesic <mesic.ajla@hotmail.com>
2019-11-01 01:15:07 +0100

    brisanje

--- backend/auction/src/main/java/com/ajla/auction/repo/UserRepo.java
+++
@@ -1,9 +0,0 @@
-package com.ajla.auction.repo;
-import com.ajla.auction.model.User;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.repository.CrudRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface UserRepo extends JpaRepository<User, Long>, UserRepoMethods {
-}
