28268f5f759815b0efbe0d61b3545deb2d612756
amesic <mesic.ajla@hotmail.com>
2019-11-21 13:37:00 +0100

    adding breadcrumbs

--- frontend/auction/src/app/app-routing.module.ts
+++ frontend/auction/src/app/app-routing.module.ts
@@ -1,11 +1,18 @@
 import { NgModule, Injectable } from "@angular/core";
-import { Routes, RouterModule, CanActivate, Router, ActivatedRouteSnapshot, RouterStateSnapshot } from "@angular/router";
+import {
+  Routes,
+  RouterModule,
+  CanActivate,
+  Router,
+  ActivatedRouteSnapshot,
+  RouterStateSnapshot
+} from "@angular/router";
 import { LoginComponent } from "./components/login/login.component";
 import { RegisterComponent } from "./components/register/register.component";
 import { LandingPageComponent } from "./components/landingPage/landing-page/landing-page.component";
 import { AllCategoriesComponent } from "./components/all-categories/all-categories.component";
 import { SingleProductPageComponent } from "./components/singleProductPage/single-product-page/single-product-page.component";
-import { LoginService } from './services/login.service';
+import { LoginService } from "./services/login.service";
 
 @Injectable()
 export class LoginRegisterActivate implements CanActivate {
@@ -15,7 +22,7 @@ export class LoginRegisterActivate implements CanActivate {
     state: RouterStateSnapshot
   ): boolean {
     if (this.loginService.isUserLoggedIn()) {
-      this.router.navigate(['/home']);
+      this.router.navigate(["/home"]);
     }
     return true;
   }
@@ -25,16 +32,50 @@ const routes: Routes = [
   { path: "", redirectTo: "/home", pathMatch: "full" },
   {
     path: "home",
+    data: {
+      breadcrumb: "HOME"
+    },
     children: [
-      { path: "login", component: LoginComponent, canActivate:[LoginRegisterActivate]},
-      { path: "register", component: RegisterComponent, canActivate:[LoginRegisterActivate] },
+      {
+        path: "login",
+        component: LoginComponent,
+        canActivate: [LoginRegisterActivate],
+        data: {
+          breadcrumb: "LOGIN"
+        }
+      },
+      {
+        path: "register",
+        component: RegisterComponent,
+        canActivate: [LoginRegisterActivate],
+        data: {
+          breadcrumb: "REGISTER"
+        }
+      },
       { path: "", component: LandingPageComponent },
-      { path: "allCategories", component: AllCategoriesComponent }
+      {
+        path: "allCategories",
+        component: AllCategoriesComponent,
+        data: {
+          breadcrumb: "ALL CATEGORIES"
+        }
+      }
     ]
   },
   {
     path: "shop",
-    children: [{ path: "product/:title/:idProduct", component: SingleProductPageComponent }]
+    data: {
+      breadcrumb: "SHOP"
+    },
+    children: [
+      {
+        path: "product/:title/:idProduct",
+        component: SingleProductPageComponent,
+        data: {
+          breadcrumb: "SINGLE PRODUCT"
+        }
+      }
+    ]
   }
 ];
 
@@ -42,5 +83,4 @@ const routes: Routes = [
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
-
 export class AppRoutingModule {}
