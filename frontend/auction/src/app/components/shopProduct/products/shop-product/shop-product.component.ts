12a8f947082bb749d7e38cd0bb75e9763b346425
amesic <mesic.ajla@hotmail.com>
2019-11-25 01:47:51 +0100

    adding list view of products

--- frontend/auction/src/app/components/shopProduct/products/shop-product/shop-product.component.ts
+++ frontend/auction/src/app/components/shopProduct/products/shop-product/shop-product.component.ts
@@ -1,25 +1,24 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, OnInit, Input } from "@angular/core";
 import { faHeart } from "@fortawesome/free-solid-svg-icons";
 import { faGavel } from "@fortawesome/free-solid-svg-icons";
+import { Router } from "@angular/router";
 
 @Component({
-  selector: 'app-shop-product',
-  templateUrl: './shop-product.component.html',
-  styleUrls: ['./shop-product.component.css']
+  selector: "app-shop-product",
+  templateUrl: "./shop-product.component.html",
+  styleUrls: ["./shop-product.component.css"]
 })
 export class ShopProductComponent implements OnInit {
-  @Input() image;
-  @Input() title;
-  @Input() description;
-  @Input() startPrice;
+  @Input() product;
   @Input() className;
 
   faHeart = faHeart;
   faGavel = faGavel;
 
-  constructor() { }
+  constructor(private router: Router) {}
 
-  ngOnInit() {
+  ngOnInit() {}
+  sendInfoOfProduct(product) {
+    this.router.navigate(["/shop/product/" + product.title + "/" + product.id]);
   }
-
 }
